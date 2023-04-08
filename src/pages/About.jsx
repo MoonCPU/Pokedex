fb44888f0563b3b5952a2598bc2d68e0b5bd4915
MoonCPU <moonkang20@gmail.com>
2023-04-08 17:13:49 -0300

    bug fix 2

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 
 import {DiReact} from 'react-icons/di';
+import {AiFillGithub} from 'react-icons/ai'
 
 import Pokeball from "../components/Pokeball";
 
@@ -21,42 +22,45 @@ const About = () => {
             exit="exit"
             transition="transition"
         >
-            <motion.div>
-                <div className="mt-4">
-                    <div>
-                        <h1 className="text-2xl">Made by</h1>
-                        <h1 className="text-3xl flex items-center justify-center">
-                            Moon Hyuk Kang
-                        </h1>                    
-                    </div>
-
-                    <div className="my-10">
-                        <h2 className="text-2xl">Tools and Technology</h2>  
-                        <ul className="max-w-sm mx-auto text-xl">
-                            <li className="flex items-center justify-center my-1">
-                                <div id="spinner">
-                                    <DiReact size={100} color="#61DBFB"/>     
-                                </div>
-                            </li>
-                            <li className="my-1">Styling - Tailwind</li>
-                            <li className="my-1">Animation - Framer Motion</li>
-                            <li className="my-1">Routing - react-router-dom</li>
-                            <li className="flex flex-row items-center justify-center my-1">
-                                API - <a href="https://pokeapi.co/" target="_blank"><Pokeball ID="mymove"/></a>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <div className="max-w-sm my-10 text-left mx-auto text-lg px-5">
-                        <p className="indent-4">This website is the second project in a roadmap of four that I have planned to develop during the first semester of 2023. The theme for this one... is a Pokedex.</p>
-                        <p className="indent-4 mt-2">Yes, there are as many Pokedex projects as there are stars in the sky. But as I had decided to focus on learning more about APIs this time, the Pokemon API was perfect for me. It's super famous in the dev community for being easy to use, even for beginners.</p>
-                        <p className="indent-4 mt-2">Anyway, I'm satisfied with the end result, even if I had to cut on some content I was originally planning for this website. I learned a lot about handling APIs and this knowledge will certainly be useful in a very near future.</p>
-                        <p className="indent-4 mt-2">Onwards to the next project! I'll see you there!</p>
-                    </div>
-                    <p className="mx-auto mt-4 text-[5px]">PS. Charizard is overrated. Blastoise or Venusaur are much cooler.</p>
+            <div className="mt-4">
+                <div>
+                    <h1 className="text-2xl">Made by</h1>
+                    <h1 className="text-3xl flex items-center justify-center">
+                        Moon Hyuk Kang
+                    </h1>                    
                 </div>
-            </motion.div> 
 
+                <div className="my-10">
+                    <h2 className="text-2xl">Tools and Technology</h2>  
+                    <ul className="max-w-sm mx-auto text-xl">
+                        <li className="flex items-center justify-center my-1">
+                            <div id="spinner">
+                                <DiReact size={100} color="#61DBFB"/>     
+                            </div>
+                        </li>
+                        <li className="my-1">Styling - Tailwind</li>
+                        <li className="my-1">Animation - Framer Motion</li>
+                        <li className="my-1">Routing - react-router-dom</li>
+                        <li className="flex flex-row items-center justify-center my-1">
+                            API - <a href="https://pokeapi.co/" target="_blank"><Pokeball ID="mymove"/></a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="max-w-sm mt-10 text-left mx-auto text-lg px-5">
+                    <p className="indent-4">This website is the second project in a roadmap of four that I have planned to develop during the first semester of 2023. The theme for this one... is a Pokedex.</p>
+                    <p className="indent-4 mt-2">Yes, there are as many Pokedex projects as there are stars in the sky. But as I had decided to focus on learning more about APIs this time, the Pokemon API was perfect for me. It's super famous in the dev community for being easy to use, even for beginners.</p>
+                    <p className="indent-4 mt-2">Anyway, I'm satisfied with the end result, even if I had to cut on some content I was originally planning for this website. I learned a lot about handling APIs and this knowledge will certainly be useful in a very near future.</p>
+                    <p className="indent-4 mt-2">Onwards to the next project! I'll see you there!</p>
+                </div>
+                <div className="flex items-center justify-center mb-4">
+                    <a href="https://github.com/MoonCPU/Pokedex" target="_blank">
+                        <AiFillGithub size={80}/>    
+                    </a>
+                </div>
+                
+                <p className="mx-auto my-2 text-[5px]">PS. Charizard is overrated. Blastoise or Venusaur are much cooler.</p>
+            </div>
         </motion.div>
     )
 }
